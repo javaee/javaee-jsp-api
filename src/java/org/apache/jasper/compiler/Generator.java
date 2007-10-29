fa9633f5fdba18d1dc744c4b8cd61a0ee3781ef2
Rajiv Mordani <mode@java.net>
2007-10-29 11:20:37 +0000

    Fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=1093 ("x-powered-by header has redundant information")
    Reviewed by: Kin-Man Chung
    
    Tests run:
    - QL
    - /cvs/glassfish/appserv-tests/devtests/web/jspXpoweredBySingleResponseHeader [ADDED]
    
    [NEWS:webcontainer 1093] x-powered-by header has redundant information
    If one JSP forwards to another, the response must not (and no longer does) contain any duplicate 'X-Powered-By: JSP' header.
    
    
    Original author: jluehe
    Date: 2006-10-09 20:43:30+00:00
    
    svn path=/trunk/; revision=597

--- src/java/org/apache/jasper/compiler/Generator.java
+++ src/java/org/apache/jasper/compiler/Generator.java
@@ -607,7 +607,7 @@ class Generator {
         out.println(");");
 
         if (ctxt.getOptions().isXpoweredBy()) {
-            out.printil("response.addHeader(\"X-Powered-By\", \"JSP/2.1\");");
+            out.printil("response.setHeader(\"X-Powered-By\", \"JSP/2.1\");");
         }
 
         out.printil(
