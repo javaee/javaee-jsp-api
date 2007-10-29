69e20c30718ed85835c6f09239435ec1996607ae
Rajiv Mordani <mode@java.net>
2007-10-29 09:46:31 +0000

    - Ported from webtier-alignment ws: updates with EL RI.   Tests: QL, JSP tck
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    Original author: kchung
    Date: 2005-06-14 16:49:49+00:00
    
    svn path=/trunk/; revision=27

--- src/trunk/src/java/com/sun/el/parser/AstLessThanEqual.java
+++ src/trunk/src/java/com/sun/el/parser/AstLessThanEqual.java
@@ -8,7 +8,7 @@ import com.sun.el.lang.EvaluationContext;
 
 /**
  * @author Jacob Hookom [jacob@hookom.net]
- * @version $Change: 181177 $$DateTime: 2001/06/26 08:45:09 $$Author: kchung $
+ * @version $Change: 181177 $$DateTime: 2001/06/26 08:45:09 $$Author: jhook $
  */
 public final class AstLessThanEqual extends BooleanNode {
     public AstLessThanEqual(int id) {
@@ -19,6 +19,12 @@ public final class AstLessThanEqual extends BooleanNode {
             throws ELException {
         Object obj0 = this.children[0].getValue(ctx);
         Object obj1 = this.children[1].getValue(ctx);
+        if (obj0 == obj1) {
+            return Boolean.TRUE;
+        }
+        if (obj0 == null || obj1 == null) {
+            return Boolean.FALSE;
+        }
         return (compare(obj0, obj1) <= 0) ? Boolean.TRUE : Boolean.FALSE;
     }
 }
