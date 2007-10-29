f06bda5050cda556555444cf3be9f26009ea6398
Rajiv Mordani <mode@java.net>
2007-10-29 11:57:09 +0000

    CDDL+GPL header updates.
    Original author: tcfujii
    Date: 2007-05-06 02:17:09+00:00
    
    svn path=/trunk/; revision=871

--- dependencies.mk
+++ dependencies.mk
@@ -34,11 +34,6 @@
 # holder.
 #
 
-#
-# Copyright 2004-2005 Sun Microsystems, Inc.  All rights reserved.
-# Use is subject to license terms.
-#
-
 CURRENT_RELEASE_TAG=S1AS8PE-DR-Final2-31_Oct_2003
 CURRENT_DEV_BRANCH=PE8FCS_BRANCH
 
