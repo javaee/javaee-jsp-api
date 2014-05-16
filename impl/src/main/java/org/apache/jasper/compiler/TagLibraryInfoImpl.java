a060cf8a16433a3f96871df2f4956601e1eca622
Kin Man Chung <kinman.chung@oracle.com>
2014-05-16 17:26:00 +0000

    Fix https://java.net/jira/browse/GLASSFISH-18391
    svn path=/trunk/; revision=1463

--- impl/src/main/java/org/apache/jasper/compiler/TagLibraryInfoImpl.java
+++ impl/src/main/java/org/apache/jasper/compiler/TagLibraryInfoImpl.java
@@ -626,7 +626,9 @@ public class TagLibraryInfoImpl extends TagLibraryInfo implements TagConstants {
 
 	if (path.startsWith("/META-INF/tags")) {
 	    // Tag file packaged in JAR
-	    ctxt.getTagFileJarUrls().put(path, jarFileUrl);
+            if (jarFileUrl != null) {
+	        ctxt.getTagFileJarUrls().put(path, jarFileUrl);
+            }
 	} else if (!path.startsWith("/WEB-INF/tags")) {
 	    err.jspError("jsp.error.tagfile.illegalPath", path);
 	}
